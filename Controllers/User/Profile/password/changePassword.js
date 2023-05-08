a52529d60d8c35e19623e4f46b42229113df8e1c
Sultan <sultanhoodie@gmail.com>
2023-05-08 14:22:14 +0800

    small fix

--- Controllers/User/Profile/password/changePassword.js
+++ Controllers/User/Profile/password/changePassword.js
@@ -24,9 +24,9 @@ const changePassword = async(req, res, next) => {
     const userId = req.userId 
     try{
         const { oldPassword, newPassword } = await verifyBody(req.body)
-        const user = await User.findByIdAndUpdate(userId, {$inc: {attempsToUpdatePassword: 1}}, {new: true, projection: {attempsToUpdatePassword: 1, password: 1}})
+        const user = await User.findByIdAndUpdate(userId, {$inc: {attemptsToUpdatePassword: 1}}, {new: true, projection: {attemptsToUpdatePassword: 1, password: 1}})
         if(!user) throw new NotFound(`User with ID ${userId} not found`)
-        else if(user.attempsToUpdatePassword > 10) throw new Forbidden("You have made too many requests to update password. Please try again later")
+        else if(user.attemptsToUpdatePassword > 10) throw new Forbidden("You have made too many requests to update password. Please try again later")
         const isMatch = await user.CheckPassword(oldPassword)
         if(!isMatch) throw new BadRequest("Password is incorrect, try again") 
         const salt = await bcrypt.genSalt()
