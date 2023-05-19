af1d39a33767b45d40d0c39e993dd5f4d52d26e3
Sult Kh <sultkh@Sults-MacBook-Air.local>
2023-05-19 17:21:47 +0800

    stats

--- Controllers/Admin/Stats/getStats.js
+++ Controllers/Admin/Stats/getStats.js
@@ -1,8 +1,13 @@
 const { StatusCodes } = require('http-status-codes')
 const Lesson = require('../../../DB/models/Lesson')
 const User = require('../../../DB/models/User')
+const {NotFound} = require('../../../Error/ErrorSamples')
+const getUrl = require('../../../helperFuncs/getUrl')
 const getStats = async (req, res, next) =>{
+    const userId = req.userId 
     try{
+        const admin = await User.findById(userId, {profilePicture: 1, name: 1})
+        if(!admin) throw new NotFound("Admin not found")
         const users = await User.find({}, {lastActive: 1, gender: 1, age: 1, isActive: 1})
         const lessons = await Lesson.find({}, {comments: 0, videos: 0, files: 0})
         let activeUsers = 0 
@@ -19,13 +24,15 @@ const getStats = async (req, res, next) =>{
                 femaleUsers += 1
             }
         }
+        admin.profilePicture = getUrl(admin.profilePicture)
         return res.status(StatusCodes.OK).json({
             lessons: lessons.length,
             totalUsers: users.length,
             maleUsers,
             femaleUsers,
             activeUsers,
-            courses: 6
+            courses: 6,
+            admin
         })
     }catch(err){
         return next(err)
