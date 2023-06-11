006131dcdd10eb8810ce93c58e53960ef4f0ce93
Sultan <sultanhoodie@gmail.com>
2023-06-11 10:17:17 +0800

    fixed bug

--- Controllers/Admin/Quotes/getAllQuotes.js
+++ Controllers/Admin/Quotes/getAllQuotes.js
@@ -3,7 +3,7 @@ const Quote = require('../../../DB/models/Quote')
 
 const getAllQuotes = async (req, res, next) => {
     try{
-        const quotes = await Quote.find()
+        const quotes = await Quote.find({isMain: false})
         return res.status(StatusCodes.OK).json({msg: "here you go", quotes})
     }catch(err){
         return next(err)
